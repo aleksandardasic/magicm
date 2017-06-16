6c07219ac2ebca789d637eea5d753d30eee3c5c7
Igor-Jovic <jovic.i24@gmail.com>
2017-06-16 07:18:51 +0000

    Added calendar service

--- app.js
+++ app.js
@@ -75,12 +75,77 @@ function getProximity(socket) {
 
     socket.emit('proximity', value);
   });
+}
 
+var fs = require('fs');
+var calendarEvents = [];
+
+function getCalendar(socket) {
+  getCalendarEntries();
+  let futureEvents = filterFutureEvents(calendarEvents);
+
+  socket.emit('calendar', futureEvents);
+}
+
+function filterFutureEvents(calendarEvents) {
+  var futureEvents = [];
+  var now = new Date();
+  var length = calendarEvents.length;
+
+  for (var i = 0; i < length; i++) {
+    var date = Date.parse(calendarEvents[i].date);
+    if (date > now) {
+      futureEvents.push(calendarEvents[i]);
+    }
+  }
+
+  futureEvents.sort(function (a, b) {
+    return Date.parse(a.date) - Date.parse(b.date);
+  });
+
+  return futureEvents;
+}
+
+function getCalendarEntries() {
+  var input = fs.createReadStream('./mockedCalendar.txt');
+  readLines(input, parseLine);
+
+  return calendarEvents;
+}
+
+function readLines(input, func) {
+  var remaining = '';
+
+  input.on('data', function (data) {
+    remaining += data;
+    var index = remaining.indexOf('\r\n');
+    while (index > -1) {
+      var line = remaining.substring(0, index);
+      remaining = remaining.substring(index + 2);
+      calendarEvents.push(func(line));
+      index = remaining.indexOf('\r\n');
+    }
+  });
+
+  input.on('end', function () {
+    if (remaining.length > 0) {
+      calendarEvents.push(func(remaining));
+    }
+  });
+}
+
+function parseLine(line) {
+  var values = line.split(',');
+  return {
+    date: values[0],
+    event: values[1]
+  }
 }
 
 io.on('connection', function (socket) {
   getTemperature(socket);
   getProximity(socket);
+  getCalendar(socket);
 });
 
 server.listen(8080);
